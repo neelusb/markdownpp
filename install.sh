7853dc1fdf21c9943baef5daaee8ed49565f7e09
neelusb <neelu.rsa@gmail.com>
2016-03-23 09:41:28 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-git clone https://www.github.com/neelusb/markdownpp.git &&
-echo Cloned into Git Repository &&
-mv markdownpp/md++.py /usr/local/bin/md++ &&
-echo Moved MarkDown++ File to /usr/local/bin &&
-chmod a+x /usr/local/bin/md++ &&
-echo Made MarkDown++ file executable &&
-rm -rf markdownpp &&
-echo Deleted all unnecessary files &&
-echo Markdown++ Installation complete! Please visit http://neelu.co/mdpp for instructions on how to use it.
