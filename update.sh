99b1eb60542e46aa5e5e0866656d3dc427248f94
neelusb <neelu.rsa@gmail.com>
2016-03-28 14:02:23 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,23 +6,23 @@ then
 else
   git clone https://www.github.com/neelusb/markdownpp.git &&
   echo Cloned into repository. &&
-  rm -f /usr/local/bin/md++ &&
-  rm -f /usr/local/bin/uninstall-md++ &&
-  rm -f /usr/local/bin/update-md++ &&
-  rm -rf /usr/local/bin/markdownpp &&
+  sudo rm -f /usr/local/bin/md++ &&
+  sudo rm -f /usr/local/bin/uninstall-md++ &&
+  sudo rm -f /usr/local/bin/update-md++ &&
+  sudo rm -rf /usr/local/bin/markdownpp &&
   echo Deleted outdated files. &&
-  mv markdownpp/md++.py /usr/local/bin/md++ &&
+  sudo mv markdownpp/md++.py /usr/local/bin/md++ &&
   echo Created MarkDown++ file. &&
   chmod +x /usr/local/bin/md++ &&
   echo Made MarkDown++ file executable &&
-  mkdir /usr/local/bin/markdownpp &&
+  sudo mkdir /usr/local/bin/markdownpp &&
   echo Created Markdown++ directory. &&
-  mv markdownpp/VERSION /usr/local/bin/markdownpp/VERSION &&
+  sudo mv markdownpp/VERSION /usr/local/bin/markdownpp/VERSION &&
   echo Updated VERSION file &&
-  mv -f markdownpp/update.sh /usr/local/bin/markdownpp/update.sh &&
-  mv -f markdownpp/uninstall.sh /usr/local/bin/markdownpp/uninstall.sh &&
-  mv -f markdownpp/installFiles/update.sh /usr/local/bin/update-md++ &&
-  mv -f markdownpp/installFiles/uninstall.sh /usr/local/bin/uninstall-md++ &&
+  sudo mv -f markdownpp/update.sh /usr/local/bin/markdownpp/update.sh &&
+  sudo mv -f markdownpp/uninstall.sh /usr/local/bin/markdownpp/uninstall.sh &&
+  sudo mv -f markdownpp/installFiles/update.sh /usr/local/bin/update-md++ &&
+  sudo mv -f markdownpp/installFiles/uninstall.sh /usr/local/bin/uninstall-md++ &&
   chmod +x /usr/local/bin/uninstall-md++ &&
   chmod +x /usr/local/bin/update-md++ &&
   echo Created Uninstall and Update files and made them executable. &&
