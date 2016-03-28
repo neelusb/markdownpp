4e8b96e808d5fa68d43ad53879413e5593895deb
neelusb <neelu.rsa@gmail.com>
2016-03-28 14:03:12 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
-rm -f /usr/local/bin/md++ &&
-rm -f /usr/local/bin/uninstall-md++ &&
-rm -f /usr/local/bin/update-md++ &&
-rm -rf /usr/local/bin/markdownpp &&
+sudo rm -f /usr/local/bin/md++ &&
+sudo rm -f /usr/local/bin/uninstall-md++ &&
+sudo rm -f /usr/local/bin/update-md++ &&
+sudo rm -rf /usr/local/bin/markdownpp &&
 echo Uninstalled MarkDown++. Please visit http://neelu.co/mdpp to download it again in the future.
